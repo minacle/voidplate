4721f8f7d93e70e503a6ae3143c61664f3f34751
Mina Her <minacle@live.com>
2025-08-11 01:33:17 +0900

    Initiate

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    java
+    alias(libs.plugins.run.paper)
+    alias(libs.plugins.shadow)
+}
+
+group = "moe.minacle.minecraft"
+version = "0.2.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "CodeMC"
+        url = uri("https://repo.codemc.io/repository/maven-public/")
+    }
+    maven {
+        name = "PaperMC"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "Sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        name = "Sonatype"
+        url = uri("https://s01.oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:${libs.versions.minecraft.get()}-R0.1-SNAPSHOT")
+    implementation(libs.bstats.bukkit)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.processResources {
+    val props = mapOf("version" to project.version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("paper-plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks {
+    runServer {
+    notCompatibleWithConfigurationCache("run-paper 2.3.1 accesses project during execution")
+        minecraftVersion(libs.versions.minecraft.get())
+    }
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    isEnableRelocation = true
+    relocationPrefix = "moe.minacle.minecraft.plugins.voidplate.shadowjar"
+    archiveClassifier.set("")
+    minimize()
+}
---
+++ src/main/java/moe/minacle/minecraft/plugins/voidplate/Plugin.java
@@ -0,0 +1,124 @@
+package moe.minacle.minecraft.plugins.voidplate;
+
+import org.jetbrains.annotations.NotNull;
+
+import org.bstats.bukkit.Metrics;
+import org.bukkit.Material;
+import org.bukkit.event.Event.Result;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.PrepareSmithingEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemRarity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.SmithingInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionType;
+
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.Equippable;
+import io.papermc.paper.datacomponent.item.PotionContents;
+import net.kyori.adventure.key.Key;
+
+public final class Plugin extends JavaPlugin implements Listener {
+
+    private static final int BSTATS_PLUGIN_ID = 26857;
+
+    @EventHandler
+    public void onPrepareSmithing(final @NotNull PrepareSmithingEvent event) {
+        final SmithingInventory smithingInventory = event.getInventory();
+        final ItemStack inputTemplate = smithingInventory.getInputTemplate();
+        final ItemStack inputEquipment;
+        final ItemStack inputMineral;
+        final Equippable inputEquipmentEquippable;
+        final ItemRarity inputEquipmentRarity;
+        final ItemStack result;
+        if (inputTemplate == null)
+            return;
+        if (inputTemplate.getType() == Material.POTION) {
+            final PotionContents potionContents = inputTemplate.getData(DataComponentTypes.POTION_CONTENTS);
+            if (potionContents == null || potionContents.potion() != PotionType.LONG_INVISIBILITY)
+                return;
+        }
+        inputMineral = smithingInventory.getInputMineral();
+        if (inputMineral == null || inputMineral.getType() != Material.ECHO_SHARD)
+            return;
+        inputEquipment = smithingInventory.getInputEquipment();
+        if (inputEquipment == null)
+            return;
+        inputEquipmentEquippable = inputEquipment.getData(DataComponentTypes.EQUIPPABLE);
+        if (inputEquipmentEquippable != null) {
+            final EquipmentSlot slot = inputEquipmentEquippable.slot();
+            if (slot != EquipmentSlot.HEAD && slot != EquipmentSlot.CHEST && slot != EquipmentSlot.LEGS && slot != EquipmentSlot.FEET)
+                return;
+        }
+        else
+            return;
+        inputEquipmentRarity = inputEquipment.getData(DataComponentTypes.RARITY);
+        if (inputEquipmentRarity != null && inputEquipmentRarity == ItemRarity.EPIC)
+            return;
+        result = inputEquipment.clone();
+        result.setData(
+            DataComponentTypes.EQUIPPABLE,
+            Equippable
+            .equippable(inputEquipmentEquippable.slot())
+            .equipSound(inputEquipmentEquippable.equipSound())
+            .assetId(Key.key("missingno"))
+            .cameraOverlay(inputEquipmentEquippable.cameraOverlay())
+            .allowedEntities(inputEquipmentEquippable.allowedEntities())
+            .dispensable(inputEquipmentEquippable.dispensable())
+            .swappable(inputEquipmentEquippable.swappable())
+            .damageOnHurt(inputEquipmentEquippable.damageOnHurt())
+            .equipOnInteract(inputEquipmentEquippable.equipOnInteract())
+            .canBeSheared(inputEquipmentEquippable.canBeSheared())
+            .shearSound(inputEquipmentEquippable.shearSound())
+            .build()
+        );
+        result.setData(DataComponentTypes.RARITY, ItemRarity.EPIC);
+        event.setResult(result);
+    }
+
+    @EventHandler
+    public void onInventoryClick(final @NotNull InventoryClickEvent event) {
+        final Inventory clickedInventory = event.getClickedInventory();
+        final ItemStack cursor;
+        final Material cursorType;
+        final int slot;
+        if (clickedInventory == null)
+            return;
+        if (clickedInventory.getType() != InventoryType.SMITHING)
+            return;
+        cursor = event.getCursor();
+        cursorType = cursor.getType();
+        slot = event.getSlot();
+        if (cursorType == Material.POTION && slot == 0) {
+            final PotionContents potionContents = cursor.getData(DataComponentTypes.POTION_CONTENTS);
+            if (potionContents == null)
+                return;
+            if (potionContents.potion() == PotionType.LONG_INVISIBILITY) {
+                event.getWhoClicked().setItemOnCursor(event.getCurrentItem());
+                event.setCurrentItem(cursor);
+                event.setResult(Result.DENY);
+                return;
+            }
+        }
+        else if (cursorType == Material.ECHO_SHARD && slot == 2) {
+            event.getWhoClicked().setItemOnCursor(event.getCurrentItem());
+            event.setCurrentItem(cursor);
+            event.setResult(Result.DENY);
+            return;
+        }
+    }
+
+    // MARK: JavaPlugin
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        new Metrics(this, BSTATS_PLUGIN_ID);
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+}
